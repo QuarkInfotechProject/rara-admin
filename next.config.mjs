41aa89be4427eaa7d67307ead7df1b3ee423e7f8
Amrit Bhusal <45762946+AmritBhusal@users.noreply.github.com>
2025-09-10 11:33:52 +0545

    configjs updated

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
   images: {
-    remotePatterns: [{ hostname: "api.communityhomestay.com" }],
+    remotePatterns: [{ hostname: "rara.api.quarkinfotech.com" }],
   },
 };
 
